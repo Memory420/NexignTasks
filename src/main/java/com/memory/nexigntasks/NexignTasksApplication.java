fbb1a743543f651fae6095ad4fc15cf326d60ae0
Memory420 <ihatesspies@gmail.com>
2025-03-21 20:04:25 +0300

    Initial Commit

---
+++ src/main/java/com/memory/nexigntasks/NexignTasksApplication.java
@@ -0,0 +1,13 @@
+package com.memory.nexigntasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexignTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexignTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memory/nexigntasks/NexignTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memory.nexigntasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexignTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
